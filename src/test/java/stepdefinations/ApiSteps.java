cf653626c5adad09505f48d7084769f27c10abe2
karthick519 <107670459+karthick519@users.noreply.github.com>
2025-05-08 16:00:07 +0530

    Update ApiSteps.java

--- src/test/java/stepdefinations/ApiSteps.java
+++ src/test/java/stepdefinations/ApiSteps.java
@@ -3,7 +3,6 @@ package stepdefinations;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,21 +12,26 @@ import io.restassured.response.Response;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ApiSteps {
-    private Response response;
 
-    private String getSecretFromKeyVault(String vaultUrl, String secretName) {
-        SecretClient secretClient = new SecretClientBuilder()
-            .vaultUrl(vaultUrl)
+    private Response response;
+    private String expectedEmail;
+    private String expectedFirstName;
+    private String expectedLastName;
+
+    private final SecretClient secretClient = new SecretClientBuilder()
+            .vaultUrl("https://<your-keyvault-name>.vault.azure.net/")
             .credential(new DefaultAzureCredentialBuilder().build())
             .buildClient();
-        return secretClient.getSecret(secretName).getValue();
-    }
 
-    @Given("I set the base URI from Azure Key Vault {string} using secret {string}")
-    public void setBaseUriFromKeyVault(String keyVaultUrl, String secretName) {
-        String baseUri = getSecretFromKeyVault(keyVaultUrl, secretName);
+    @Given("I retrieve {string} from Azure Key Vault")
+    public void getSecretFromKeyVault(String secretName) {
+        String baseUri = secretClient.getSecret(secretName).getValue();
         RestAssured.baseURI = baseUri;
-        System.out.println("Base URI from Key Vault: " + baseUri);
+
+        // Load expected values too (optional: cache to avoid repeat calls)
+        expectedEmail = secretClient.getSecret("expectedEmail").getValue();
+        expectedFirstName = secretClient.getSecret("expectedFirstName").getValue();
+        expectedLastName = secretClient.getSecret("expectedLastName").getValue();
     }
 
     @When("I send a GET request to {string}")
@@ -41,11 +45,10 @@ public class ApiSteps {
         response.then().statusCode(statusCode);
     }
 
-    @Then("the response should contain the user's details")
-    public void verifyUserDetails() {
-        response.then().body("data.id", equalTo(2));
-        response.then().body("data.email", equalTo("janet.weaver@reqres.in"));
-        response.then().body("data.first_name", equalTo("Janet"));
-        response.then().body("data.last_name", equalTo("Weaver"));
+    @Then("the response should match the details stored in Azure Key Vault")
+    public void verifyUserDetailsFromKeyVault() {
+        response.then().body("data.email", equalTo(expectedEmail));
+        response.then().body("data.first_name", equalTo(expectedFirstName));
+        response.then().body("data.last_name", equalTo(expectedLastName));
     }
 }
