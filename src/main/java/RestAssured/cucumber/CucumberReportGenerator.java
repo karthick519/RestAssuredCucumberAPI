886d93bbc0d92579a397bc1044889511c1aeee69
karthick519 <107670459+karthick519@users.noreply.github.com>
2025-05-12 16:16:23 +0530

    Update CucumberReportGenerator.java

--- src/main/java/RestAssured/cucumber/CucumberReportGenerator.java
+++ src/main/java/RestAssured/cucumber/CucumberReportGenerator.java
@@ -1,19 +1,15 @@
-package RestAssured.cucumber;
-
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-
 import java.io.File;
 import java.util.Collections;
-import java.util.List;
 
 public class CucumberReportGenerator {
     public static void main(String[] args) {
         File reportOutputDirectory = new File("target/cucumber-reports");
-        List<String> jsonFiles = Collections.singletonList("target/cucumber.json");
+        String jsonReportPath = "target/cucumber.json";
 
-        Configuration config = new Configuration(reportOutputDirectory, "RestAssured API Tests");
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, config);
+        Configuration config = new Configuration(reportOutputDirectory, "RestAssured Tests");
+        ReportBuilder reportBuilder = new ReportBuilder(Collections.singletonList(jsonReportPath), config);
         reportBuilder.generateReports();
     }
 }
