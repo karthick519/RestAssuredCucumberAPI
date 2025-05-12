1109e43d3b7b0d3423fd81bcfd8ae9835e411ff3
karthick519 <107670459+karthick519@users.noreply.github.com>
2025-05-12 14:39:10 +0530

    Update TestRunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -1,17 +1,17 @@
+package testrunners;
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "classpath:features",                 // Correct way to refer to feature files
-    glue = {"stepdefinations"},                      // Your package is named stepdefinations
-    plugin = {
-        "pretty",
-        "json:target/cucumber.json"
-    },
+    features = "src/test/resources/features/Example.feature",
+    glue = {"stepdefinations"},
+    plugin = {"pretty", "html:target/cucumber-report/cucumber-reports.html", "json:target/cucumber-report/Cucumber.json","rerun:target/rerun.txt" },
     monochrome = true
-)
 
+)
 public class TestRunner {
+
 }
