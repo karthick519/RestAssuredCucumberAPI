1bcd395e537aa29eb24d9a2886fbb8304629a8fb
karthick519 <107670459+karthick519@users.noreply.github.com>
2025-05-12 14:05:46 +0530

    Update TestRunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -1,6 +1,16 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"stepdefinations"},
-    plugin = {"pretty", "json:target/cucumber.json"},  // Needed for HTML report
+    plugin = {
+        "pretty",
+        "json:target/cucumber.json"
+    },
     monochrome = true
 )
+public class TestRunner {
+}
